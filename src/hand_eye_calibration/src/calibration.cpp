cd942e2fce49892f411d6d542dcfcfba744a2bd5
sean85914 <sean19960914@gmail.com>
2019-05-16 18:07:57 +0800

    finish hand-eye calibration

--- src/hand_eye_calibration/src/calibration.cpp
+++ src/hand_eye_calibration/src/calibration.cpp
@@ -1,41 +1,73 @@
 #include <fstream>
+#include <boost/filesystem.hpp>
 #include <ros/ros.h>
 #include <ros/package.h>
 #include <tf/transform_listener.h>
 #include <geometry_msgs/Pose.h>
 #include <arm_operation/target_pose.h>
 
+/*
+ *  Hand-eye automatically calibration, collecting data process
+ *  
+ *  Parameters:
+ *    dx: x difference in meter
+ *    dz: z difference in meter
+ *    tag_frame: target tag frame
+ *    file_name: output file name
+ *  Service: /ur3_control_server/ur_control/goto_pose
+ *  Frame: base_link, ee_link, tcp_link, tag_frame
+ */
+
 class calibration{
  private:
-  int count;
-  double dx, dz;
-  std::fstream fs;
-  std::string package_path;
-  std::string file_path;
-  ros::NodeHandle nh_;
-  ros::NodeHandle pnh_;
-  ros::ServiceClient client;
-  geometry_msgs::Pose original_pose;
-  arm_operation::target_pose req;
-  tf::TransformListener listener;
+  int count; // Counter for process
+  double dx, dz; // Predefined path distance
+  std::fstream fs; // file stream
+  std::string tag_frame; // frame id for tag, from parameter server
+  std::string package_path; // package path
+  std::string file_name; // file name, from parameter server
+  std::string file_path; // file path = package path + folder + file name + file extension
+  ros::NodeHandle nh_; // public node handler
+  ros::NodeHandle pnh_; // private node handler
+  ros::ServiceClient client; // service client
+  geometry_msgs::Pose original_pose; // first robot arm pose
+  arm_operation::target_pose req; // request for server
+  tf::TransformListener listener; 
+  /*
+   *  Write the data to file
+   *  format: x(hand coord.) y(hand coord.) z(hand coord.) x(eye_coord) y(eye_coord) z(eye_coord)
+   *  [param]in geometry_msgs::Pose p_tcp:
+   *  [param]in geometry_msgs::Pose p_tag:
+   */
   void write_data(geometry_msgs::Pose p_tcp, geometry_msgs::Pose p_tag){
     fs << p_tcp.position.x << " "
        << p_tcp.position.y << " "
-       << p_tcp.position.z << "|\t"
+       << p_tcp.position.z << " "
        << p_tag.position.x << " "
        << p_tag.position.y << " "
        << p_tag.position.z << "\n";
   }
+  /*
+   *  Get tag transformation in eye coordinate
+   *  [param]in tf::StampedTransform &t: transform reference
+   *  [param]out bool: 0 if cannot get transform, 1 otherwise
+   */
   bool get_tag_data(tf::StampedTransform &t){
     try{
-      listener.waitForTransform("camera_link", "tag_0", ros::Time(0), ros::Duration(2.0));
-      listener.lookupTransform("camera_link", "tag_0", ros::Time(0), t);
+      listener.waitForTransform("camera_link", tag_frame, ros::Time(0), ros::Duration(2.0));
+      listener.lookupTransform("camera_link", tag_frame, ros::Time(0), t);
     } catch(tf::TransformException ex){
       ROS_ERROR("%s", ex.what());
       return 0;
     }
     return 1;
   }
+  /*
+   *  Get tag transformation in hand coordinate
+   *  [param]in tf::StampedTransform &t: transform reference
+   *  [param]in std::string target: which frame we want, either \ee_link\ or \tcp_link\
+   *  [param]out bool: 0 if cannot get transform, 1 otherwise
+   */
   bool get_arm_data(tf::StampedTransform &t, std::string target){
     try{
       listener.waitForTransform("base_link", target, ros::Time(0), ros::Duration(0.5));
@@ -46,6 +78,11 @@ class calibration{
     }
     return 1;
   }
+  /*
+   *  Convert stamped transform to pose
+   *  [param]in tf::StampedTransform t: transform to convert
+   *  [param]out geometry_msgs::Pose: pose that encode the information of t
+   */
   geometry_msgs::Pose transform2Pose(tf::StampedTransform t){
     geometry_msgs::Pose res;
     res.position.x = t.getOrigin().getX();
@@ -57,6 +94,15 @@ class calibration{
     res.orientation.w = t.getRotation().getW();
     return res;
   }
+  /*
+   *  Calibration process, the robot arm will follow the predefined path and receive data
+   *  Predefined path:
+   *    0  3  4  7  8
+   *     --         |
+   *     dx      dz |
+   *                |
+   *    1  2  5  6  9
+   */
   void calibration_process(void){
     while(count<10){
       ROS_INFO("----------- %d -----------", count);
@@ -64,32 +110,14 @@ class calibration{
         case 0:
           req.request.target_pose = original_pose;
           break;
-        case 1:
+        case 1: case 5: case 9:
           req.request.target_pose.position.z -= dz;
           break;
-        case 2:
+        case 2: case 4: case 6: case 8:
           req.request.target_pose.position.x += dx;
           break;
-        case 3:
+        case 3: case 7:
           req.request.target_pose.position.z += dz;
-          break;
-        case 4:
-          req.request.target_pose.position.x += dx;
-          break;
-        case 5:
-          req.request.target_pose.position.z -= dz;
-          break;
-        case 6:
-          req.request.target_pose.position.x += dx;
-          break;
-        case 7:
-          req.request.target_pose.position.z += dz;
-          break;
-        case 8:
-          req.request.target_pose.position.x += dx;
-          break;
-        case 9:
-          req.request.target_pose.position.z -= dz;
           break;
       }
       client.call(req);
@@ -97,37 +125,47 @@ class calibration{
       tf::StampedTransform st;
       get_tag_data(st);
       geometry_msgs::Pose p_tag = transform2Pose(st);
-      get_arm_data(st, "tcp_link");
+      get_arm_data(st, "tcp_link"); // Remember to broadcast tcp_link frame
       geometry_msgs::Pose p_tcp = transform2Pose(st);
       write_data(p_tcp, p_tag);
       ++count;
     }
-    ROS_INFO("Go home...");
+    ROS_INFO("Go home..."); // back to first pose
     req.request.target_pose = original_pose;
     client.call(req);
     ros::Duration(1.0).sleep();
   }
   
  public:
+  // Constructor
   calibration(ros::NodeHandle nh, ros::NodeHandle pnh): nh_(nh), pnh_(pnh), count(0){
     req.request.factor = 0.5; 
-    package_path = ros::package::getPath("arm_operation");
-    file_path = package_path + "/data/calibration.txt";
-    fs.open(file_path, std::fstream::out | std::fstream::app); // Write file
-    if(!fs.is_open()) {ROS_ERROR("Cannot open file!"); ros::shutdown();}
+    // Get parameters
     if(!pnh_.getParam("dx", dx)) dx = 0.03; ROS_INFO("dx: %f", dx);
     if(!pnh_.getParam("dz", dz)) dz = 0.05; ROS_INFO("dz: %f", dz);
+    if(!pnh_.getParam("tag_frame", tag_frame)) tag_frame = "tag_0"; ROS_INFO("tag frame is: %s", tag_frame.c_str());
+    if(!pnh_.getParam("file_name", file_name)) file_name = "calibration"; ROS_INFO("file_name: %s", file_name.c_str());
+    package_path = ros::package::getPath("hand_eye_calibration");
+    file_path = package_path + "/data/" + file_name + ".txt";
+    // Check if data directory exist
+    boost::filesystem::path p(package_path+"/data");
+    if(!boost::filesystem::exists(p)){
+      ROS_INFO("Directory doesnot exist, creating one...");
+      boost::filesystem::create_directory(p);
+    }
+    fs.open(file_path, std::fstream::out | std::fstream::app); // Write file at the end
+    if(!fs.is_open()) {ROS_ERROR("Cannot open file!"); ros::shutdown();}
     ros::service::waitForService("/ur3_control_server/ur_control/go_straight"); ROS_INFO("Service connect!");
     client = nh_.serviceClient<arm_operation::target_pose>("/ur3_control_server/ur_control/goto_pose", true);
     ROS_INFO("Start process");
     tf::StampedTransform transform;
     get_arm_data(transform, "ee_link");
     original_pose = transform2Pose(transform);
-    fs << "Robot  position: |\t\t tag position\n";
     calibration_process();
     ROS_INFO("End process");
     ros::shutdown();
   }
+  // Desctructor
   ~calibration(){
     fs.close();
   }
---
+++ src/hand_eye_calibration/src/get_transform.cpp
@@ -0,0 +1,142 @@
+#include <cstdio> // printf
+#include <iostream> // std::cout
+#include <fstream> // std::ifstream
+#include <Eigen/Dense> // Eigen core, SVD
+#include <tf/tf.h> // Transformation
+
+/*
+ *  Get transformation from two point sets in given file using ICP for hand-eye calibration
+ *  
+ *  Editor: Sean Lu
+ *  Last edited: 5/16, 2019
+ */
+
+/*
+ *  Parse file and return target and source point sets
+ *  [param]in char* file: file name
+ *  [param]in Eigen::MatrixXf &target: target point set matrix reference
+ *  [param]in Eigen::MatrixXf &source: source point set matrix reference
+ *  [param]out: 1 if successfully load file, 0 otherwise
+ */ 
+bool parse_file(char*, Eigen::MatrixXf&, Eigen::MatrixXf&);
+/*
+ *  Compute the best transformation from given two sets
+ *  Process: 
+ *    1. Compute centroids
+ *    2. Compute deviation
+ *    3. Compute covariance matrix of two sets
+ *    4. SVD among covariance and get U, V matrices
+ *    5. Compute rotation matrix and translation, refer to: https://blog.csdn.net/zhouyelihua/article/details/77807541
+ *  [param]in Eigen::MatrixXf &target: target point set matrix reference
+ *  [param]in Eigen::MatrixXf &source: source point set matrix reference
+ *  [param]in tf::Transform &t: transformation placeholder
+ */
+void compute(Eigen::MatrixXf&, Eigen::MatrixXf&, tf::Transform&);
+/*
+ *  Get best transformation from given file, will call \parse_file\ then \compute\
+ *  [param]in char* file: file name
+ *  [param]in tf::Transform &t: transformation placeholder
+ *  [param]out bool: 1 if successfully compute the result, 0 otherwise
+ */
+bool get_best_transformation(char*, tf::Transform&);
+
+int main(int argc, char** argv)
+{
+  if(argc!=2) {
+    printf("Not enough input, please provide input file...\n");
+    return -1;
+  }
+  tf::Transform T;
+  if(!get_best_transformation(argv[1], T)) return -1;
+  double r, p, y;
+  T.getBasis().getRPY(r, p, y);
+  std::cout << "----------------------------------------------------\n";
+  std::cout << "Translation: ["
+            << T.getOrigin().getX() << ", " << T.getOrigin().getY() << ", " << T.getOrigin().getZ() << "]\n"
+            << "Orientation: (Quaternion) ["  
+            << T.getRotation().getX() << ", " << T.getRotation().getY() << ", "
+            << T.getRotation().getZ() << ", " << T.getRotation().getW() << "]\n"
+            << "Orientation: (Euler) [" 
+            << r << ", " << p << ", " << y << "]\n";
+  return 0;
+}
+
+bool parse_file(char *file, Eigen::MatrixXf &target, Eigen::MatrixXf &source){
+  std::ifstream f;
+  std::string line;
+  int count = 0;
+  f.open(file, std::ifstream::in);
+  if(!f){
+    printf("Cannot open file! Abort...\n");
+    return 0;
+  }
+  while(std::getline(f, line)) ++count;
+  target = Eigen::MatrixXf(count, 3);
+  source = Eigen::MatrixXf(count, 3);
+  // return the cursor to the begining of the file
+  f.clear();
+  f.seekg(0, std::ios::beg);
+  int row = 0;
+  while(!f.eof() && row<count){
+    std::getline(f, line);
+    std::stringstream ss(line);
+    int i = 0;
+    while(ss.good() && i < 6){
+      double val;
+      switch(i){
+        case 0: case 1: case 2:
+          ss >> val; target(row, i) = val; break;
+        case 3: case 4: case 5:
+          ss >> val; source(row, i-3) = val; break;
+      } ++i;
+    } ++row;
+  }
+  return 1;
+}
+
+void compute(Eigen::MatrixXf &target, Eigen::MatrixXf &source, tf::Transform &t){
+  // Compute centroid
+  Eigen::Vector3f target_centroid, source_centroid;
+  double target_sum = 0, source_sum = 0;
+  for(int i=0; i<3; ++i){
+    for(int j=0; j<target.rows(); ++j){
+      target_sum += target(j, i);
+      source_sum += source(j, i);
+    }
+    target_centroid(i) = target_sum/(double)target.rows();
+    source_centroid(i) = source_sum/(double)source.rows();
+    target_sum = source_sum = 0;
+  }
+  // Compute deviation
+  Eigen::MatrixXf target_deviation(target.rows(), 3),
+                  source_deviation(source.rows(), 3);
+  for(int i=0; i<target.rows(); ++i){
+    for(int j=0; j<3; ++j){
+      target_deviation(i, j) = target(i, j) - target_centroid(j);
+      source_deviation(i, j) = source(i, j) - source_centroid(j);
+    }
+  }
+  // Compute H
+  Eigen::MatrixXf H(3, 3); H = source_deviation.transpose() * target_deviation;
+  // SVD
+  Eigen::JacobiSVD<Eigen::MatrixXf> svd(H, Eigen::ComputeThinU | Eigen::ComputeThinV);
+  // Compute rotation and translation
+  Eigen::Matrix3f R = svd.matrixV() * svd.matrixU().transpose();
+  Eigen::Array3f trans = target_centroid - R * source_centroid;
+  tf::Matrix3x3 rot(R(0, 0), R(0, 1), R(0, 2),
+                    R(1, 0), R(1, 1), R(1, 2),
+                    R(2, 0), R(2, 1), R(2, 2));
+  tf::Vector3 vec(trans(0), trans(1), trans(2));
+  t = tf::Transform(rot, vec);
+  // Eigen::Matrix4f
+  Eigen::Matrix4f homo = Eigen::Matrix4f::Zero();
+  homo.block<3, 3>(0, 0) = R; homo.block<3, 1>(0, 3) = trans; homo(3, 3) = 1.0;
+  std::cout << "Homogeneous transformation matrix: \n" << homo << "\n";
+}
+
+bool get_best_transformation(char* file, tf::Transform& t){
+  Eigen::MatrixXf target, source;
+  if(!parse_file(file, target, source)) return 0;
+  compute(target, source, t);
+  return 1;
+}
