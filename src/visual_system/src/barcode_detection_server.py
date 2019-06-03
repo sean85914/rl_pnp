a398b894884ecb36f4af870375e387e0af5ab448
sean85914 <sean19960914@gmail.com>
2019-06-04 01:50:40 +0800

    fix bug, add publish image for visualization

--- src/visual_system/src/barcode_detection_server.py
+++ src/visual_system/src/barcode_detection_server.py
@@ -32,6 +32,7 @@ class SSDPrediction(object):
 		self.bridge = CvBridge()
 		self.image1 = None
 		self.image2 = None
+		self.image_to_plot = None
 		self.rospack = rospkg.RosPack()
 		pkg_path = self.rospack.get_path('visual_system')
 		self.model_name = rospy.get_param("~model_name", "ssd300_BARCODE_21500.pth")
@@ -45,6 +46,7 @@ class SSDPrediction(object):
 			self.net.cuda()
 		self.sub_img1 = rospy.Subscriber("/camera1/color/image_raw", Image, self.img1_cb)
 		self.sub_img2 = rospy.Subscriber("/camera2/color/image_raw", Image, self.img2_cb)
+		self.pub_img_with_bb = rospy.Publisher("~prediction_res", Image, queue_size = 10)
 		self.detection_service = rospy.Service("~barcode_detection", barcode_detect, self.service_cb)
 
 		rospy.loginfo("Finish loading weights...")
@@ -64,15 +66,18 @@ class SSDPrediction(object):
 	# Service callback
 	def service_cb(self, req):
 		res = barcode_detectResponse()
+		res.result = False
 		if(req.camera_id): # camera1
 			if(self.image1 is not None):
 				self.img_to_net = self.img_preprocessing(self.image1)
+				self.image_to_plot = self.image1
 			else:
 				rospy.logerr("No image receive from camera 1 yet, aborting...")
 				return res
 		else: # camera 2
 			if(self.image2 is not None):
 				self.img_to_net = self.img_preprocessing(self.image2)
+				self.image_to_plot = self.image2
 			else:
 				rospy.logerr("No image receive from camera 2 yet, aborting...")
 				return res
@@ -81,14 +86,33 @@ class SSDPrediction(object):
 			var = var.cuda()
 		prediction_res = self.net(var) # Forward pass
 		detection = prediction_res.data
+		scale = torch.Tensor(self.image_to_plot.shape[1::-1]).repeat(2)
 		for i in range(detection.size(1)):
-			det = detection[0, i, 0, 0].cpu().numpy()
-			if det >= self.confidence_thres:
-				rospy.loginfo("Detect barcode")
-				res.result = true
-			else:
-				rospy.loginfo("No barcode detected")
-				res.result = false
+			j = 0
+			while detection[0, i, j, 0].cpu().numpy() >= self.confidence_thres:
+				res.result = True
+				score = detection[0, i, j, 0]
+				display_txt = '%d'%(score*100.)
+				pt = (detection[0, i, j, 1:]*scale).cpu().numpy()
+				pt = [int(p) for p in pt]
+				# Draw rectangle
+				cv2.rectangle(self.image_to_plot, (pt[0], pt[1]), (pt[2], pt[3]), \
+						(0, 0, 255), 3)
+				# Put text
+				font = cv2.FONT_HERSHEY_SIMPLEX
+				cv2.putText(self.image_to_plot, display_txt, (pt[0], pt[1]), font, \
+						3, (255, 0, 0), cv2.LINE_AA)
+				j += 1
+		if(res.result == True):
+			rospy.loginfo("Detect barcode")
+		else:	rospy.loginfo("No barcode detected")
+		# Publish drawed image
+		try:
+			self.image_to_plot = cv2.cvtColor(self.image_to_plot, cv2.COLOR_BGR2RGB)
+			self.pub_img_with_bb.publish(self.bridge.cv2_to_imgmsg(self.image_to_plot))
+		except CvBridgeError as e:
+			print(e)
+
 		return res
 
 	# Preprocessing for SSD
