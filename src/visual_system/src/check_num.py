cdc2fd0a5a8620877e829d96fc0fa8cb2fdd74ec
sean85914 <sean19960914@gmail.com>
2019-06-28 10:42:07 +0800

    valid if less than given num

--- src/visual_system/src/check_num.py
+++ src/visual_system/src/check_num.py
@@ -2,9 +2,11 @@ import numpy as np
 import cv2
 import argparse
 
-parser = argparse.ArgumentParser(description="Check nonzeros entries of difference of two images in given path")
-parser.add_argument('--path', help="path to images", type=str)
+parser = argparse.ArgumentParser(description='Check nonzeros entries of difference of two images in given path')
+parser.add_argument('--path', help='path to images', type=str)
 parser.add_argument('--num', help='total amount of images', type=int)
+parser.add_argument('--height', help='Depth difference greater than this value will be consider as change, in millimeter', type=float)
+parser.add_argument('--verbose', help='if print detail information', type=bool, default=False)
 
 args = parser.parse_args()
 
@@ -19,12 +21,22 @@ if __name__ == "__main__":
         # Convert uint16 to int16
         img1_int = np.array(img1, dtype=np.int16)
         img2_int = np.array(img2, dtype=np.int16)
+        # Workspace
         subimg_1 = img1_int[21:245, 208:452]
         subimg_2 = img2_int[21:245, 208:452]
         result_img = subimg_1 - subimg_2
-        non_zero_num = np.count_nonzero(result_img)
+        # Old: count non zero
+        '''non_zero_num = np.count_nonzero(result_img)
         print non_zero_num
-        arr.append(non_zero_num)
+        arr.append(non_zero_num)'''
+        cnt = 0
+        for i in result_img:
+            for j in i:
+                if j<-args.height: 
+                    cnt += 1
+                    if args.verbose: print j
+        print cnt
+        arr.append(cnt)
     print "Mean: {}".format(int(np.mean(arr)))
     print "Max:  {}".format(np.max(arr))
     print "Min:  {}".format(np.min(arr))
