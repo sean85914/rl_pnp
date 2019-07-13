b05482bec19f89947d73b7fd273656e2377f9102
sean85914 <sean19960914@gmail.com>
2019-07-14 03:51:43 +0800

    add plot logged data script

---
+++ src/grasp_suck/src/plot_data.py
@@ -0,0 +1,78 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import argparse
+
+def parse_data(string, list_):
+	idx = string.find(" ")
+	sub_str = string[idx+1:]
+	while True:
+		idx = sub_str.find(" ")
+		if idx == -1:
+			break
+		sub_str = sub_str[idx+1:]
+	try:
+		data = float(sub_str)
+	except ValueError:
+		data = int(sub_str)
+	list_.append(data)
+	return list_
+
+parser = argparse.ArgumentParser(prog="visual_suck_and_grasp", description="Plit logged data")
+parser.add_argument("--episode", type=int, help="Episodes trained")
+parser.add_argument("--data_path", type=str, help="Path to logged data")
+parser.add_argument("--img_name", type=str, help="Name of saved image")
+
+args = parser.parse_args()
+
+trained_episode = args.episode
+
+iterations = []
+return_ = []
+loss_mean = []
+num_invalid = []
+num_valid = []
+ratio = [] # Seems redundant
+action_ratio = [] # \frac{valid_action}{Total iter}
+
+for i in range(trained_episode):
+	file_name = args.data_path + "logger_{}/curve.txt".format(i)
+	f = open(file_name, 'r')
+	s = f.readline()
+	iterations = parse_data(s, iterations)
+	s = f.readline()
+	return_ = parse_data(s, return_)
+	s = f.readline()
+	loss_mean = parse_data(s, loss_mean)
+	s = f.readline()
+	num_invalid = parse_data(s, num_invalid)
+	s = f.readline()
+	num_valid = parse_data(s, num_valid)
+	s = f.readline()
+	ratio = parse_data(s, ratio)
+
+for idx in range(len(iterations)):
+	val = float(num_valid[idx])/iterations[idx]
+	action_ratio.append(val)
+
+plt.figure(figsize=(16,9))
+plt.subplot(3,2,1)
+plt.plot(iterations)
+plt.title("Iterations")
+plt.subplot(3,2,2)
+plt.plot(loss_mean)
+plt.title("Mean Loss")
+plt.subplot(3,2,3)
+plt.plot(num_invalid)
+plt.title("Number of Invalid Action")
+plt.subplot(3,2,4)
+plt.plot(num_valid)
+plt.title("Number of Valid Action")
+plt.subplot(3,2,5)
+plt.plot(return_)
+plt.title("Return at t=0")
+plt.subplot(3,2,6)
+plt.plot(action_ratio)
+plt.title("Valid Action / Total Iterations")
+
+plt.savefig("%s.png" %args.img_name, dpi=100)
+#plt.show()
