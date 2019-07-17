b5dba8438ead0a36c59d22fdde9d240eed0ba29d
sean85914 <sean19960914@gmail.com>
2019-07-17 20:54:38 +0800

    seems more stable

--- src/vacuum_conveyor_control/arduino_code/vacuum_conveyor/vacuum_conveyor.ino
+++ src/vacuum_conveyor_control/arduino_code/vacuum_conveyor/vacuum_conveyor.ino
@@ -15,6 +15,7 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial.setTimeout(100);
   pinMode(VACUUM_GENERATOR, OUTPUT);
   pinMode(VACUUM_BREAKER, OUTPUT);
   pinMode(CONVRYOR_CONTROL, OUTPUT);
@@ -23,45 +24,59 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Serial.available()){
+  /*if(Serial.available()){
     char command_type = Serial.read();
     if(command_type == 'v'){
-      int command = Serial.read() - '0';
-      switch(command){
-        case 0:
-          digitalWrite(VACUUM_GENERATOR, LOW);
-          delay(1);
-          digitalWrite(VACUUM_BREAKER, LOW);
-          Serial.println("v: 0");
-          break;
-        case 1:
-          digitalWrite(VACUUM_GENERATOR, LOW);
-          delay(1);
-          digitalWrite(VACUUM_BREAKER, HIGH);
-          Serial.println("v: 1");
-          break;
-        case 2:
-          digitalWrite(VACUUM_GENERATOR, HIGH);
-          delay(1);
-          digitalWrite(VACUUM_BREAKER, LOW);
-          Serial.println("v: 2");
-          break;
-        case 3:
-          digitalWrite(VACUUM_GENERATOR, HIGH);
-          delay(1);
-          digitalWrite(VACUUM_BREAKER, HIGH);
-          Serial.println("v: 3");
-          break;
+      if(Serial.available()){
+        int command = Serial.read() - '0';
+        switch(command){
+          case 0:
+            digitalWrite(VACUUM_GENERATOR, LOW);
+            delay(1);
+            digitalWrite(VACUUM_BREAKER, LOW);
+            break;
+          case 1:
+            digitalWrite(VACUUM_GENERATOR, LOW);
+            delay(1);
+            digitalWrite(VACUUM_BREAKER, HIGH);
+            break;
+          case 2:
+            digitalWrite(VACUUM_GENERATOR, HIGH);
+            delay(1);
+            digitalWrite(VACUUM_BREAKER, LOW);
+            break;
+          case 3:
+            digitalWrite(VACUUM_GENERATOR, HIGH);
+            delay(1);
+            digitalWrite(VACUUM_BREAKER, HIGH);
+            break;
+        }
       }
     }
     else if(command_type == 'c'){
       digitalWrite(CONVRYOR_CONTROL, HIGH);
       delay(100);
       digitalWrite(CONVRYOR_CONTROL, LOW);
-      //Serial.println("c: 1");
     }
     else if(command_type == 'p'){
-      int command = Serial.read() - '0';
+      if(Serial.available()){
+        int command = Serial.read() - '0';
+        switch(command){
+          case 0:
+            digitalWrite(PNEUMATIC_CYLINDER, LOW);
+            break;
+          case 1:
+            digitalWrite(PNEUMATIC_CYLINDER, HIGH);
+            break;
+        }
+      }
+    }
+  }*/
+  if(Serial.available()){
+    String command_str = Serial.readString();
+    char command_type = command_str[0];
+    if(command_type=='p'){
+      int command = command_str[1]-'0';
       switch(command){
         case 0:
           digitalWrite(PNEUMATIC_CYLINDER, LOW);
@@ -70,6 +85,36 @@ void loop() {
           digitalWrite(PNEUMATIC_CYLINDER, HIGH);
           break;
       }
+    }
+    else if(command_type=='v'){ 
+      int command = command_str[1]-'0';
+      switch(command){
+        case 0:
+          digitalWrite(VACUUM_GENERATOR, LOW);
+          delay(1);
+          digitalWrite(VACUUM_BREAKER, LOW);
+          break;
+        case 1:
+          digitalWrite(VACUUM_GENERATOR, LOW);
+          delay(1);
+          digitalWrite(VACUUM_BREAKER, HIGH);
+          break;
+        case 2:
+          digitalWrite(VACUUM_GENERATOR, HIGH);
+          delay(1);
+          digitalWrite(VACUUM_BREAKER, LOW);
+          break;
+        case 3:
+          digitalWrite(VACUUM_GENERATOR, HIGH);
+          delay(1);
+          digitalWrite(VACUUM_BREAKER, HIGH);
+          break;
+      }
+    }
+    else if(command_type=='c'){
+      digitalWrite(CONVRYOR_CONTROL, HIGH);
+      delay(100);
+      digitalWrite(CONVRYOR_CONTROL, LOW);
     }
   }
   delay(10);
