7d8950cc4ae363a27c4e2bacd4c12ac7a74a90ef
sean85914 <sean19960914@gmail.com>
2019-07-23 01:03:12 +0800

    add pointcloud to heightmap, angle mapping

--- src/grasp_suck/src/utils.py
+++ src/grasp_suck/src/utils.py
@@ -1,7 +1,14 @@
 import numpy as np
 import cv2
+import struct
+import ctypes
 from cv_bridge import CvBridge, CvBridgeError
-from visual_system.srv import get_image, get_imageRequest, get_imageResponse
+import sensor_msgs.point_cloud2 as pc2
+from visual_system.srv import get_pc, get_pcRequest, get_pcResponse
+
+workspace_limits = np.asarray([[-0.65, -0.30], [-0.23, 0.12], [-0.01, 0.2]])
+heightmap_resolution = 0.35/224
+angle_map = [np.radians(90), np.radians(45), np.radians(0), np.radians(-45)] # TODO: the angle mapping may not correct, should test!
 
 # cv bridge
 br = CvBridge()
@@ -17,7 +24,7 @@ br = CvBridge()
                         |___/                                       
 '''
 
-# Get color and depth images from service response and save them
+'''# Get color and depth images from service response and save them
 def get_imgs_from_msg(response, img_path, iteration):
 	color = br.imgmsg_to_cv2(response.crop_color_img)
 	depth = br.imgmsg_to_cv2(response.crop_depth_img)
@@ -26,6 +33,42 @@ def get_imgs_from_msg(response, img_path, iteration):
 	cv2.imwrite(color_name, color)
 	cv2.imwrite(depth_name, depth)
 	return color, depth
+'''
+
+def get_heightmap(pc, img_path, iteration):
+	color_heightmap = np.zeros((224, 224, 3), dtype=np.uint8)
+	depth_heightmap = np.zeros((224, 224))
+	points = np.zeros((224, 224, 3)) 
+	gen = pc2.read_points(pc, skip_nans=True)
+	int_data = list(gen)
+	for p in int_data:
+		rgb = p[3]
+		s = struct.pack(">f", rgb)
+		i = struct.unpack(">l", s)[0]
+		pack = ctypes.c_uint32(i).value
+		r = (pack & 0x00FF0000) >> 16
+		g = (pack & 0x0000FF00) >> 8
+		b = (pack & 0x000000FF)
+		heightmap_x = np.floor((p[0]-workspace_limits[0][0])/heightmap_resolution).astype(int)
+		heightmap_y = np.floor((p[1]-workspace_limits[1][0])/heightmap_resolution).astype(int)
+		color_heightmap[heightmap_y, heightmap_x, 0] = b
+		color_heightmap[heightmap_y, heightmap_x, 1] = g
+		color_heightmap[heightmap_y, heightmap_x, 2] = r
+		depth_heightmap[heightmap_y, heightmap_x] = p[2]
+		z_bot = workspace_limits[2][0]
+		depth_heightmap = depth_heightmap - z_bot
+		#depth_heightmap[depth_heightmap<0] = 0
+		#depth_heightmap[depth_heightmap == -z_bot] = np.nan
+		points[heightmap_y, heightmap_x] = np.array([p[0], p[1], p[2]])
+	color_name = img_path + "color_{:06}.jpg".format(iteration)
+	depth_name = img_path + "depth_{:06}.png".format(iteration)
+	cv2.imwrite(color_name, color_heightmap)
+	depth_cp = np.copy(depth_heightmap)
+	depth_cp = depth_cp - np.min(depth_heightmap)
+	depth_cp[depth_cp<0] = 0
+	depth_cp = (depth_cp*1000).astype(np.uint16)
+	cv2.imwrite(depth_name, depth_heightmap)
+	return color_heightmap, depth_heightmap, points
 
 # Draw symbols in the color image with different primitive
 def draw_image(image, primitive, pixel_index):
@@ -33,7 +76,7 @@ def draw_image(image, primitive, pixel_index):
 	result = cv2.circle(image, center, 7, (0, 0, 0), 2)
 	X = 20
 	Y = 7
-	theta = np.radians(-90.0+45.0*pixel_index[0])
+	theta = angle_map[pixel_index[0]]#np.radians(-90.0+45.0*pixel_index[0])
 	x_unit = [ np.cos(theta), np.sin(theta)]
 	y_unit = [-np.sin(theta), np.cos(theta)]
 	if not primitive: # GRASP
@@ -88,12 +131,12 @@ def epsilon_greedy_policy(epsilon, suck_predictions, grasp_predictions):
 		else: # GRASP
 			tmp = np.where(grasp_predictions == np.max(grasp_predictions))
 			pixel_index = [tmp[0][0], tmp[1][0], tmp[2][0]]
-			angle = np.radians(-90+45*pixel_index[0])
+			angle = angle_map[pixel_index[0]]#np.radians(-90+45*pixel_index[0])
 	else:
 		if not explore: # GRASP
 			tmp = np.where(grasp_predictions == np.max(grasp_predictions))
 			pixel_index = [tmp[0][0], tmp[1][0], tmp[2][0]]
-			angle = np.radians(-90+45*pixel_index[0])
+			angle = angle_map[pixel_index[0]]#np.radians(-90+45*pixel_index[0])
 		else:
 			action = 1 # SUCK
 			action_str = 'suck'
@@ -115,7 +158,7 @@ def greedy_policy(suck_predictions, grasp_predictions):
 	else: # GRASP
 		tmp = np.where(grasp_predictions == np.max(grasp_predictions))
 		pixel_index = [tmp[0][0], tmp[1][0], tmp[2][0]]
-		angle = np.radians(-90+45*pixel_index[0])
+		angle = angle_map[pixel_index[0]]#np.radians(-90+45*pixel_index[0])
 	return action, action_str, pixel_index, angle
 
 # Policy that only choose grasp
@@ -124,7 +167,7 @@ def grasp_only_policy(grasp_predictions):
 	action_str = 'grasp'
 	tmp = np.where(grasp_predictions == np.max(grasp_predictions))
 	pixel_index = [tmp[0][0], tmp[1][0], tmp[2][0]]
-	angle = np.radians(-90+45*pixel_index[0])
+	angle = angle_map[pixel_index[0]]#np.radians(-90+45*pixel_index[0])
 	return pixel_index, angle
 
 def standarization(suck_predictions, grasp_predictions):
