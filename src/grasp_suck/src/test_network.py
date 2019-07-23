ce2be865c08a8e9d81262ca230f37a547a6b132d
sean85914 <sean19960914@gmail.com>
2019-07-24 03:04:26 +0800

    update offline testing code

--- src/grasp_suck/src/test_network.py
+++ src/grasp_suck/src/test_network.py
@@ -4,56 +4,53 @@ import time
 #from threading import Thread, currentThread
 import numpy as np
 import cv2
+import rospy
 import argparse
 import torch
+import utils
+from visual_system.srv import get_pc
 
 from trainer import Trainer
 
-parser = argparse.ArgumentParser(prog="visual_suck_and_grasp", description="using color and depth images to do pick and place")
-parser.add_argument("--is_testing", action="store_true", default=False)
-parser.add_argument("--force_cpu",  action="store_true", default=False)
-parser.add_argument("--last_model", type=str, default="", help="If provided, continue training with provided model file")
-
-args = parser.parse_args()
-
 # Parameter
-testing      = args.is_testing
-use_cpu      = args.force_cpu
+testing      = True
+use_cpu      = False
 episode      = 0
 iteration    = 0
 suck_reward  = 1.0
 grasp_reward = 1.0
 discount     = 0.5
-Z_THRES      = 0.645
-num_of_items = 10
-save_every   = 5
+
+get_pc_client = rospy.ServiceProxy("/pixel_to_xyz/get_pc", get_pc)
 
 trainer = Trainer(suck_reward, grasp_reward, discount, testing, use_cpu)
-
-color = cv2.imread("test_img/color_000000.jpg")
-next_color = cv2.imread("test_img/color_000001.jpg")
-depth = cv2.imread("test_img/depth_000000.png", -1)
-next_depth = cv2.imread("test_img/depth_000001.png", -1)
+print "Loading model..."
+trainer.model.load_state_dict(torch.load("../training_log/logger_64/models/final.pth"))
+print "Complete!"
 
 total = 0
 ts = time.time()
+res = get_pc_client()
+print "get result: {}".format(time.time()-ts)
+total += time.time()-ts
+ts = time.time()
+color, depth, points = utils.get_heightmap(res.pc, "", 0)
+print "get heightmap: {}".format(time.time()-ts)
+total += time.time()-ts
+ts = time.time()
+
 suck_predictions, grasp_predictions, state_feat = \
                           trainer.forward(color, depth, is_volatile=True)
 print "Forward: {}".format(time.time()-ts)
 total += time.time()-ts
 
-action_str = "grasp"
-action_success = False
+suck_predictions, grasp_predictions = utils.standarization(suck_predictions, grasp_predictions)
+suck_heatmap = utils.vis_affordance(suck_predictions[0])
+cv2.imwrite("suck.jpg", suck_heatmap)
+for i in range(len(grasp_predictions)):
+	grasp_heatmap = utils.vis_affordance(grasp_predictions[i])
+	name = "grasp_{}.jpg".format(i)
+	cv2.imwrite(name, grasp_heatmap)
 
-ts = time.time()
-label_value, prev_reward_value = trainer.get_label_value(action_str, action_success, \
-		                                                         next_color, next_depth)
-print "Get label: {}".format(time.time()-ts)
-total += time.time()-ts
-pixel_index = [1, 112, 112]
-ts = time.time()
-trainer.backprop(color, depth, action_str, pixel_index, label_value)
-print "Backpropagation: {}".format(time.time()-ts)
-total += time.time()-ts
-
-print "Total time: {}".format(total)
+cv2.imshow("color", color)
+cv2.waitKey(0)
