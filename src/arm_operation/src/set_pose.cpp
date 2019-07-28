190a37cc7943aca9e478e78b837058c387ed07ff
sean85914 <sean19960914@gmail.com>
2019-07-28 16:42:37 +0800

    check if invalid file given

--- src/arm_operation/src/set_pose.cpp
+++ src/arm_operation/src/set_pose.cpp
@@ -4,14 +4,17 @@
 #include <std_msgs/Int16.h>
 #include <arm_operation/joint_pose.h>
 
-/* Read the waypoints file and make UR traverse go to pose of user-specific index in the file
+/* Read the waypoints file and make UR go to pose of user-specific index in the file
  * Subscribe:
  *   ~go_index (std_msgs::Int16): index to go
  * Service client:
- *   /ur3_control_server/ur_control/goto_joint_pose
+ *   /urX_control_server/ur_control/goto_joint_pose
  * Parameter:
- *   ~file_nameL file name with extension
- * Last modify: 4/25, 2019
+ *   ~type: which robot to use, either 3 or 5
+ *   ~file_name: file name with extension
+ * Last modify: 7/28, 2019
+ * Changelog:
+ *  7/28: revise hardcoded service name and add robot choosing parameter, abort invalid files
  * Editor: Sean
  */
 
@@ -25,10 +28,12 @@ class GoToWP {
   ros::NodeHandle nh_, pnh_;
   // Subscriber
   ros::Subscriber sub_index;
-  // Parameter
+  // Parameters
+  bool type; // 0: ur3; 1: ur5
   std::string file_name;
+  std::string service_name;
   // Service client
-  ros::ServiceClient ur3_goto_joint_pose_client_;
+  ros::ServiceClient goto_joint_pose_client_;
   // Parse input file to double matrix array
   bool parse_file(){
     std::string line;
@@ -39,7 +44,7 @@ class GoToWP {
     fp.open(file_name.c_str(), std::ifstream::in);
     if(!fp){
       ROS_ERROR("Can't open file.");
-      return -1;
+      return 0;
     }
     while(std::getline(fp, line)){
       ++counter;
@@ -54,12 +59,13 @@ class GoToWP {
     fp.seekg(0, std::ios::beg);
     while(!fp.eof()){
       std::getline(fp, line);
+      if(line.empty()) break;
       std::stringstream ss(line);
       int i = 0;
       while(ss.good() && i < 6){
         ss >> res[index][i];
         ++i;
-      }
+      } if(i!=6) {ROS_ERROR("Not enough joints given, please check your file!"); return 0;}
       ++index;
     }
     // Close the file
@@ -71,16 +77,19 @@ class GoToWP {
     for(int i=0; i<6; ++i){
       js_req.request.joint[i] = wp_list[msg.data][i];
     }
-    ur3_goto_joint_pose_client_.call(js_req); ros::Duration(0.3).sleep();
+    goto_joint_pose_client_.call(js_req); ros::Duration(0.3).sleep();
   }
  public:
   GoToWP(ros::NodeHandle nh, ros::NodeHandle pnh): nh_(nh), pnh_(pnh){
     sub_index = pnh_.subscribe("index_to_go", 1, &GoToWP::cbCallback, this);
+    if(!pnh_.getParam("type", type)) {type=true;}
+    ROS_INFO("%s", (type==0?"Use UR3":"Use UR5"));
+    service_name = "/ur" + std::to_string((type==0?3:5)) + "_control_server/ur_control/goto_joint_pose";
     if(!pnh_.getParam("file_name", file_name)) {ROS_ERROR("No file provide, exit..."); ros::shutdown();}
     if(!parse_file()){ros::shutdown(); return;}
     ROS_INFO("Get %d waypoints", wp_len);
-    ur3_goto_joint_pose_client_ = nh_.serviceClient<arm_operation::joint_pose>("/ur3_control_server/ur_control/goto_joint_pose");
-    while(!ros::service::waitForService("/ur3_control_server/ur_control/goto_joint_pose", ros::Duration(3.0)) and ros::ok()){
+    goto_joint_pose_client_ = nh_.serviceClient<arm_operation::joint_pose>(service_name);
+    while(!ros::service::waitForService(service_name, ros::Duration(3.0)) and ros::ok()){
       ROS_WARN("Service not available, still waiting...");
     }
   }
