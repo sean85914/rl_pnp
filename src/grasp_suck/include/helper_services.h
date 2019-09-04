19f07d8719ec7e6cb874a4f7d1567ce8eb62d21e
huangjuite <huangjuite@gmail.com>
2019-09-04 03:51:41 -0400

    change constants, fix sort None error

--- src/grasp_suck/include/helper_services.h
+++ src/grasp_suck/include/helper_services.h
@@ -27,7 +27,7 @@
 #define SUCK  true
 
 const int    REPEAT_TIME = 2;
-const double OFFSET = -0.022f; // Lower 1 centimeter to make sure have contract with object
+const double OFFSET = -0.027f; // Lower 1 centimeter to make sure have contract with object
 const double X_OFFSET = 0.0f; // Seems error from hand-eye calibration
 
 class Helper_Services{
