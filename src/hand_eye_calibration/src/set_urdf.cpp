1a663c254fbfd25f47169ad2c687d6382a09896f
sean85914 <sean19960914@gmail.com>
2019-09-26 04:25:00 +0800

    add input search key to ensure revise in right place

--- src/hand_eye_calibration/src/set_urdf.cpp
+++ src/hand_eye_calibration/src/set_urdf.cpp
@@ -5,17 +5,26 @@
 int main(int argc, char** argv)
 {
   // ./set_urdf [x] [y] [z] [roll] [pitch] [yaw]
-  if(argc!=7){
+  if(argc!=8){
     std::cout << "\033[1;31mNot enough input arguments, abort...\033[0m\n";
-    std::cout << "Example usage: ./set_urdf [x] [y] [z] [roll] [yaw] [pitch]\n";
+    std::cout << "Example usage: ./set_urdf [joint_name] [x] [y] [z] [roll] [yaw] [pitch]\n";
     exit(EXIT_FAILURE);
   }
-  double x = atof(argv[1]),
-         y = atof(argv[2]),
-         z = atof(argv[3]),
-         roll = atof(argv[4]),
-         pitch = atof(argv[5]),
-         yaw = atof(argv[6]);
+  // INFO
+  double x = atof(argv[2]),
+         y = atof(argv[3]),
+         z = atof(argv[4]),
+         roll = atof(argv[5]),
+         pitch = atof(argv[6]),
+         yaw = atof(argv[7]);
+  std::cout << "Info:\n" 
+            << "\tJoint name: " << argv[1] << "\n"
+            << "\tx: "     << x << "\n"
+            << "\ty: "     << y << "\n"
+            << "\tz: "     << z << "\n"
+            << "\troll: "  << roll << "\n"
+            << "\tpitch: " << pitch << "\n"
+            << "\tyaw: "   << yaw << "\n";
   std::string package_path = ros::package::getPath("robot_description");
   std::string urdf_path = package_path + "/urdf/system.urdf.xacro";
   tinyxml2::XMLDocument doc;
@@ -32,14 +41,21 @@ int main(int argc, char** argv)
    */
   tinyxml2::XMLElement *root = doc.RootElement(),
                        *joint = root->FirstChildElement("joint"),
-                       *origin = joint->FirstChildElement("origin");
-  if(root==NULL or joint==NULL or origin==NULL){
+                       *origin;
+  if(root==NULL or joint==NULL){
     std::cout << "Can't parse XARCO correctly, abort\033[0m\n";
     exit(EXIT_FAILURE);
   }
+  while(strcmp(joint->Attribute("name"), argv[1])!=0){
+    joint = joint->NextSiblingElement("joint");
+  }
+  if(joint==NULL){
+    std::cout << "Can't parse XARCO correctly, abort\033[0m\n";
+    exit(EXIT_FAILURE);
+  }
+  origin = joint->FirstChildElement("origin");
   std::string xyz_string = std::to_string(x)    + " " + std::to_string(y)     + " " + std::to_string(z),
               rpy_string = std::to_string(roll) + " " + std::to_string(pitch) + " " + std::to_string(yaw);
-  std::cout << origin->Attribute("xyz") << "\n" << origin->Attribute("rpy") << "\n";
   std::cout << "xyz set from: [" << origin->Attribute("xyz") << "] to [" << xyz_string << "]\n"
             << "rpy set from: [" << origin->Attribute("rpy") << "] to [" << rpy_string << "]\n";
   origin->SetAttribute("xyz", xyz_string.c_str());
