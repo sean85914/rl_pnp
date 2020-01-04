0ae884391d00c60519e02b814bfa29783af12396
sean85914 <sean19960914@gmail.com>
2020-01-05 04:59:01 +0800

    add transition without priority fiven (as algorithm in the paper)

--- src/grasp_suck/src/prioritized_memory.py
+++ src/grasp_suck/src/prioritized_memory.py
@@ -3,40 +3,43 @@ import random
 import pickle
 from SumTree import SumTree
 
+# Revised from: https://github.com/rlcode/per and https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow/blob/master/contents/5.2_Prioritized_Replay_DQN/RL_brain.py
+
 class Memory:
 	epsilon = 0.01
 	alpha = 0.6
 	beta = 0.4
 	beta_increment = 0.001
+	abs_err_upper = 1.
 	
 	def __init__(self, capacity):
 		self.tree = SumTree(capacity)
-		self.capacity = capacity
 	
 	def _get_priority(self, error):
 		return (np.abs(error) + self.epsilon) ** self.alpha
 		
-	def add(self, error, transition):
-		p = self._get_priority(error)
-		self.tree.add(p, transition)
+	def add(self, transition):
+		max_p = np.max(self.tree.tree[-self.tree.capacity:])
+		if max_p == 0:
+			max_p = self.abs_err_upper
+		self.tree.add(max_p, transition)
 		
 	def sample(self, n):
 		batch = []
 		idxs = []
-		segment = self.tree.total() / n
+		segment = self.tree.total / n
 		priorities = []
 		
 		self.beta = np.min([1., self.beta + self.beta_increment])
 		
 		for i in range(n):
-			a = segment * i
-			b = segment * (i + 1)
+			a, b = segment * i, segment * (i + 1)
 			s = random.uniform(a, b)
 			idx, p, data = self.tree.get(s)
 			priorities.append(p)
 			batch.append(data)
 			idxs.append(idx)
-		sampling_probabilities = priorities / self.tree.total() # (1)
+		sampling_probabilities = priorities / self.tree.total # (1)
 		is_weight = np.power(self.tree.n_entries * sampling_probabilities, -self.beta)
 		is_weight /= is_weight.max() # 3.4
 		
