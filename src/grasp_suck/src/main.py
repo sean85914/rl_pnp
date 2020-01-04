f1abfeacb223abf820daec435d9167659b220a7e
sean85914 <sean19960914@gmail.com>
2020-01-05 05:00:55 +0800

    simplify code

--- src/grasp_suck/src/main.py
+++ src/grasp_suck/src/main.py
@@ -11,8 +11,8 @@ import utils
 import pickle
 import serial
 from trainer import Trainer
-from collections import namedtuple, deque
 from prioritized_memory import Memory
+from utils import Transition
 # srv
 from std_srvs.srv import SetBool, SetBoolRequest, SetBoolResponse, \
                          Empty, EmptyRequest, EmptyResponse
@@ -21,9 +21,6 @@ from visual_system.srv import get_pc, get_pcRequest, get_pcResponse, \
                               pc_is_empty, pc_is_emptyRequest, pc_is_emptyResponse, \
                               check_grasp_success, check_grasp_successRequest, check_grasp_successResponse
 from visualization.srv import viz_marker, viz_markerRequest, viz_markerResponse
-
-# Define transition tuple
-Transition = namedtuple('Transition', ['color', 'depth', 'pixel_idx', 'reward', 'next_color', 'next_depth', 'is_empty'])
 
 # Parse argument
 parser = argparse.ArgumentParser(prog="reinforcement_grasping", description="Reinforcement learning for robot arm grasping")
@@ -42,26 +39,12 @@ parser.add_argument("--save_every", type=int, default=10, help="Every how many s
 parser.add_argument("--learning_rate", type=float, default=1e-4, help="Learning rate for the trainer, default is 1e-4")
 args = parser.parse_args()
 utils.show_args(args)
+testing, episode, use_cpu, model_str, buffer_str, epsilon, port, buffer_size, learning_freq, updating_freq, mini_batch_size, save_every, learning_rate = utils.parse_input(args)
 
-testing         = args.is_testing
-episode         = args.episode
-use_cpu         = args.force_cpu
-model_str       = args.model
-buffer_str      = args.buffer_file
-epsilon         = args.epsilon if not testing else 1
-port            = args.port
-buffer_size     = args.buffer_size
-learning_freq   = args.learning_freq
-updating_freq   = args.updating_freq
-mini_batch_size = args.mini_batch_size
-save_every      = args.save_every
-learning_rate   = args.learning_rate
 # Constant
 reward = 1.0
 discount_factor = 0.5
 iteration = 0
-t = 0
-return_ = 0.0
 z_thres = -0.017
 program_ts = time.time()
 memory = Memory(buffer_size)
@@ -71,8 +54,7 @@ if model_str == "" and testing: # TEST SHOULD PROVIDE MODEL
 	print "\033[0;31mNo model provided, exit!\033[0m"
 	os._exit(0)
 	
-if buffer_str != "":
-	memory.load_memory(buffer_file)
+if buffer_str != "": memory.load_memory(buffer_file)
 
 # trainer
 trainer = Trainer(reward, discount_factor, use_cpu, learning_rate)
@@ -111,6 +93,32 @@ explore_list  = [] # If using explore?
 return_list   = [] # Episode return
 episode_list  = [] # Episode step length
 
+# Code for calling service
+def _get_pc(iteration, is_before):
+	get_pc_req = get_pcRequest()
+	if is_before:
+		get_pc_req.file_name = pc_path + "{:06}_before.pcd".format(iteration)
+	else:
+		get_pc_req.file_name = pc_path + "{:06}_after.pcd".format(iteration)
+	return get_pc_client(get_pc_req)
+def _viz(point, action, angle, valid):
+	viz_req = viz_markerRequest()
+	viz_req.point = utils.point_np_to_ros(point)
+	viz_req.primitive = action
+	viz_req.angle = angle
+	viz_req.valid = valid
+	viz(viz_req)
+def _take_action(tool_id, point, angle):
+	agent_action = agent_abb_actionRequest()
+	agent_action.tool_id = tool_id
+	agent_action.position = utils.point_np_to_ros(point)
+	agent_action.angle = angle
+	_ = agent_take_action_client(agent_action)
+def _check_if_empty(pc):
+	empty_checker_req = pc_is_emptyRequest()
+	empty_checker_req.input_pc = pc
+	return empty_checker(empty_checker_req).is_empty.data
+
 # Initialize
 go_home()
 vacuum_pump_control(SetBoolRequest(False))
@@ -120,23 +128,14 @@ try:
 		if iteration is not 0: arduino.write("gb 1000") # Green + buzzer for alarming resetting
 		cmd = raw_input("\033[1;34m[%f] Reset environment, if ready, press 's' to start. 'e' to exit: \033[0m" %(time.time()-program_ts))
 		if cmd == 'E' or cmd == 'e':
-			utils.saveFiles(action_list, target_list, result_list, loss_list, explore_list, return_list, episode_list, position_list, csv_path)
-			# Save experience buffer
-			memory.save_memory(csv_path)
-			print "Regular shutdown"
-			sys.exit(0)
+			utils.shutdown_process(action_list, target_list, result_list, loss_list, explore_list, return_list, episode_list, position_list, csv_path, memory, True)
 		elif cmd == 'S' or cmd == 's':
-			episode_list.append(t)
-			t = 0
-			if iteration is not 0: return_list.append(return_)
-			return_ = 0.0
-			is_empty = False
+			if iteration is not 0: return_list.append(return_); episode_list.append(t)
+			t = 0; return_ = 0.0; is_empty = False
 			while is_empty is not True:
 				print "\033[0;32m[%f] Iteration: %d\033[0m" %(time.time()-program_ts, iteration)
 				if not testing: epsilon_ = max(epsilon * np.power(0.998, t), 0.1) # half after 350 steps
-				get_pc_req = get_pcRequest()
-				get_pc_req.file_name = pc_path + "{:06}_before.pcd".format(iteration)
-				pc_response = get_pc_client(get_pc_req)
+				pc_response = _get_pc(iteration, True)
 				color, depth, points = utils.get_heightmap(pc_response.pc, image_path, depth_path, iteration)
 				ts = time.time()
 				suck_1_prediction, suck_2_prediction, grasp_prediction = trainer.forward(color, depth, is_volatile=True)
@@ -160,20 +159,16 @@ try:
 				suck_2_prediction = utils.standarization(suck_2_prediction)
 				for i in range(len(grasp_prediction)):
 					grasp_prediction[i] = utils.standarization(grasp_prediction[i])
-				explore = False
 				if not testing: # Train
 					explore, action, action_str, pixel_index, angle = utils.epsilon_greedy_policy(epsilon_, suck_1_prediction, suck_2_prediction, grasp_prediction, depth, diff_path, iteration)
 				else: # Testing
-					action, action_str, pixel_index, angle = utils.greedy_policy(suck_1_prediction, suck_2_prediction, grasp_prediction)
+					action, action_str, pixel_index, angle = utils.greedy_policy(suck_1_prediction, suck_2_prediction, grasp_prediction); explore = False
 				explore_list.append(explore)
 				target_list.append(pixel_index)
 				position_list.append(points[pixel_index[1], pixel_index[2]])
 				del suck_1_prediction, suck_2_prediction, grasp_prediction
-				print "Take action [\033[1;31m%s\033[0m] at (%d, %d) -> (%f, %f, %f)" %(action_str, pixel_index[1], pixel_index[2], \
-				                                                                        points[pixel_index[1], pixel_index[2], 0],
-				                                                                        points[pixel_index[1], pixel_index[2], 1],
-				                                                                        points[pixel_index[1], pixel_index[2], 2])
-				# Save color heightmap + prediction heatmap + motion primitive and corresponding position, then show it
+				utils.print_action(action_str, pixel_index, points[pixel_index[1], pixel_index[2]])
+				# Save (color heightmap + prediction heatmap + motion primitive and corresponding position), then show it
 				visual_img = utils.draw_image(mixed_imgs[pixel_index[0]], explore, pixel_index)
 				img_name = vis_path + "vis_{:06}.jpg".format(iteration)
 				cv2.imwrite(img_name, visual_img)
@@ -182,26 +177,11 @@ try:
 				# Check if action valid (is NAN?)
 				is_valid = utils.check_if_valid(points[pixel_index[1], pixel_index[2]])
 				# Visualize in RViz
-				viz_req = viz_markerRequest()
-				viz_req.point.x = points[pixel_index[1], pixel_index[2], 0]
-				viz_req.point.y = points[pixel_index[1], pixel_index[2], 1]
-				viz_req.point.z = points[pixel_index[1], pixel_index[2], 2]
-				viz_req.primitive = action
-				viz_req.angle = angle
-				viz_req.valid = is_valid
-				viz(viz_req)
+				_viz(points[pixel_index[1], pixel_index[2]], action, angle, is_valid)
 				if is_valid: # Only take action if valid
-					action_list.append(action)
-					agent_action = agent_abb_actionRequest()
-					if pixel_index[0] == 0: # suck_1 -> 3
-						agent_action.tool_id = 3
-					elif pixel_index[0] == 1: # suck_2 -> 2
-						agent_action.tool_id = 2
-					else:
-						agent_action.tool_id = 1 # grasp
-					agent_action.position = viz_req.point
-					agent_action.angle = angle
-					agent_response = agent_take_action_client(agent_action)
+					# suck_1(0) -> 3, suck_2(1) -> 2, other(2~5) (grasp) -> 1
+					tool_id = (3-pixel_index[0]) if pixel_index[0] < 2 else 1
+					_take_action(tool_id, points[pixel_index[1], pixel_index[2]], angle)
 					go_home()
 				else: # invalid
 					action_list.append(-1); arduino.write("r 1000") # Red
@@ -213,22 +193,20 @@ try:
 						check_grasp_success_request = check_grasp_successRequest()
 						check_grasp_success_request.prior_pcd_str = pc_path + "{:06}_before.pcd".format(iteration)
 						# Get temporary pc and save file
-						get_pc_req.file_name = pc_path + "{:06}_after.pcd".format(iteration)
-						tmp_response = get_pc_client(get_pc_req)
+						_ = _get_pc(iteration, False)
 						check_grasp_success_request.post_pcd_str = pc_path + "{:06}_after.pcd".format(iteration)
-						check_grasp_success_request.operated_position = viz_req.point
+						check_grasp_success_request.operated_position = utils.point_np_to_ros(points[pixel_index[1], pixel_index[2]])
 						action_success = check_grasp_success(check_grasp_success_request).is_success
 				result_list.append(action_success)
 				if action_success: go_place(); arduino.write("g 1000") # Green
-				else: vacuum_pump_control(SetBoolRequest(False)); arduino.write("o 1000") # Orange
+				else: vacuum_pump_control(SetBoolRequest(False))
+				if is_valid: arduino.write("o 1000") # Orange
 				time.sleep(0.5)
 				# Get next images, and check if bin is empty
-				get_pc_req.file_name = pc_path + "{:06}_after.pcd".format(iteration)
-				next_pc = get_pc_client(get_pc_req).pc
-				next_color, next_depth, next_points = utils.get_heightmap(next_pc, image_path + "next_", depth_path + "next_", iteration)
-				empty_checker_req = pc_is_emptyRequest()
-				empty_checker_req.input_pc = next_pc
-				is_empty = empty_checker(empty_checker_req).is_empty.data
+				next_pc = _get_pc(iteration, False)
+				next_color, next_depth, next_points = utils.get_heightmap(next_pc.pc, image_path + "next_", depth_path + "next_", iteration)
+				is_empty = _check_if_empty(next_pc.pc)
+				# TODO: form a function
 				# Reward shaping
 				# Success -> +R
 				# Fail and z>=z_thres -> -R
@@ -245,20 +223,12 @@ try:
 				return_ += current_reward * np.power(discount_factor, t) 
 				print "\033[1;33mCurrent reward: {} \t Return: {}\033[0m".format(current_reward, return_)
 				# Store transition to experience buffer
-				color_name = image_path + "color_{:06}.jpg".format(iteration)
-				depth_name = depth_path + "depth_data_{:06}.txt".format(iteration)
-				next_color_name = image_path + "next_color_{:06}.jpg".format(iteration)
-				next_depth_name = depth_path + "next_depth_data_{:06}.txt".format(iteration)
+				color_name, depth_name, next_color_name, next_depth_name = utils.wrap_strings(image_path, depth_path, iteration)
 				transition = Transition(color_name, depth_name, pixel_index, current_reward, next_color_name, next_depth_name, is_empty)
-				# Compute TD error
-				td_target = trainer.get_label_value(current_reward, next_color, next_depth, is_empty)
-				rotation_idx = pixel_index[0]-2 if "grasp" in action_str else -1
-				old_value = trainer.forward(color, depth, action_str, False, rotation_idx)[0, pixel_index[1], pixel_index[2]]
-				memory.add((td_target-old_value)**2, transition)
-				iteration += 1
-				t += 1
-				if iteration % learning_freq == 0: # Update parameters
-					arduino.write("b 1000")
+				memory.add(transition)
+				iteration += 1; t += 1
+				if iteration % learning_freq == 0: # Training stage, update parameters
+					back_ts = time.time(); arduino.write("b 1000")
 					mini_batch, idxs, is_weight = memory.sample(mini_batch_size)
 					sampled_iter = []
 					for i in range(mini_batch_size):
@@ -270,7 +240,7 @@ try:
 						next_color = cv2.imread(mini_batch[i].next_color)
 						next_depth = np.loadtxt(mini_batch[i].next_depth)
 						td_target = trainer.get_label_value(mini_batch[i].reward, next_color, next_depth, mini_batch[i].is_empty)
-						trainer.backprop(color, depth, pixel_index, td_target, is_weight)
+						trainer.backprop(color, depth, pixel_index, td_target, is_weight[i])
 					print "Sampled at iteration: ", sampled_iter
 					# After parameter updated, update prioirites tree
 					for i in range(mini_batch_size):
@@ -288,7 +258,7 @@ try:
 							action_str = "grasp"; rotate_idx = pixel_index[0] -2
 						old_value = trainer.forward(color, depth, action_str, False, rotate_idx)[0, pixel_index[1], pixel_index[2]]
 						memory.update(idxs[i], (td_target-old_value)**2)
-					arduino.write("b 1000"); print "\n[%f] Complete" %(time.time()-program_ts)
+					arduino.write("b 1000"); print "Backpropagation& Updating: {} seconds".format(time.time()-back_ts)
 				if iteration % updating_freq == 0:
 					print "[%f] Replace target network to behavior network" %(time.time()-program_ts)
 					trainer.target_net.load_state_dict(trainer.behavior_net.state_dict)
@@ -298,12 +268,4 @@ try:
 					print "[%f] Model: %s saved" %(time.time()-program_ts, model_name)
 					
 except KeyboardInterrupt:
-	utils.saveFiles(action_list, target_list, result_list, loss_list, explore_list, return_list, episode_list, position_list, csv_path)
-	# Save experience buffer
-	memory.save_memory(csv_path)
-	model_name = model_path + "interrupt_{}.pth".format(iteration)
-	torch.save(trainer.behavior_net.state_dict(), model_name)
-	utils.saveFiles(action_list, target_list, result_list, loss_list, explore_list, return_list, episode_list, position_list, csv_path)
-	print "[%f] Model: %s saved" %(time.time()-program_ts, model_name)
-	print "Shutdown since keyboard interrupt"
-	sys.exit(0)
+	utils.shutdown_process(action_list, target_list, result_list, loss_list, explore_list, return_list, episode_list, position_list, csv_path, memory, False)
