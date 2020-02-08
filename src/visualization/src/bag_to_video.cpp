3da19e3213dc8346ea9bf76ab60fcb1a39c39982
sean85914 <sean19960914@gmail.com>
2020-02-08 15:00:49 +0800

    convert faster

--- src/visualization/src/bag_to_video.cpp
+++ src/visualization/src/bag_to_video.cpp
@@ -1,3 +1,6 @@
+// STD
+#include <chrono>
+// Boost
 #include <boost/foreach.hpp>
 // ROS
 #include <rosbag/bag.h>
@@ -28,24 +31,18 @@ Usage: ./bag_to_video [bag_name] [output_video_name]\n\033[0m\
     exit(EXIT_FAILURE);
   }
   rosbag::View view(bag);
+  std::vector<cv::Mat> img_vec;
+  cv_bridge::CvImagePtr cv_bridge_ptr;
   double bag_duration = (view.getEndTime() - view.getBeginTime()).toSec();
   // Get number of frame
   int img_num = 0, width, height;
+  auto s_ts = std::chrono::high_resolution_clock::now();
   foreach(const rosbag::MessageInstance m, view){
     if(m.getDataType()=="sensor_msgs/Image"){
       ++img_num;
       sensor_msgs::Image::ConstPtr img_ptr = m.instantiate<sensor_msgs::Image>();
       width = img_ptr->width;
       height = img_ptr->height;
-    }
-  }
-  std::string output_filename(argv[2]);
-  output_filename+=".avi";
-  cv::VideoWriter video(output_filename, CV_FOURCC('M', 'J', 'P', 'G'), img_num/bag_duration, cv::Size(width, height));
-  cv_bridge::CvImagePtr cv_bridge_ptr;
-  foreach(const rosbag::MessageInstance m, view){
-    if(m.getDataType()=="sensor_msgs/Image"){
-      sensor_msgs::Image::ConstPtr img_ptr = m.instantiate<sensor_msgs::Image>();
       try{
         cv_bridge_ptr = cv_bridge::toCvCopy(img_ptr);
       } catch(cv_bridge::Exception& e){
@@ -54,8 +51,16 @@ Usage: ./bag_to_video [bag_name] [output_video_name]\n\033[0m\
       }
       cv::Mat rgb_img;
       cv::cvtColor(cv_bridge_ptr->image, rgb_img, CV_BGR2RGB);
-      video << rgb_img;
+      img_vec.push_back(rgb_img);
     }
   }
+  std::string output_filename(argv[2]);
+  output_filename+=".avi";
+  cv::VideoWriter video(output_filename, CV_FOURCC('M', 'J', 'P', 'G'), img_num/bag_duration, cv::Size(width, height));
+  for(int i=0; i<img_vec.size(); ++i)
+    video << img_vec[i];
+  auto e_ts = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(e_ts-s_ts).count();
+  std::cout << "Conversion time: " << duration*1e-9 << " seconds\n";
   return 0;
 }
