912bb73f9eea9327e7f79cd104aece81499f5ca8
sean85914 <sean19960914@gmail.com>
2020-03-03 09:30:35 +0800

    reorder block

--- src/grasp_suck/src/utils.py
+++ src/grasp_suck/src/utils.py
@@ -10,7 +10,6 @@ import multiprocessing as mp
 from scipy import ndimage
 from collections import namedtuple
 import matplotlib.pyplot as plt
-from cv_bridge import CvBridge, CvBridgeError
 import sensor_msgs.point_cloud2 as pc2
 from geometry_msgs.msg import Point
 from visual_system.srv import get_pc, get_pcRequest, get_pcResponse
@@ -34,8 +33,6 @@ resolution = data['resolution']
 
 workspace_limits = np.asarray([[x_lower, x_upper], [y_lower, y_upper], [z_lower, z_upper]]) # X Y Z
 heightmap_resolution = (workspace_limits[0][1]-workspace_limits[0][0])/resolution
-# cv bridge
-br = CvBridge()
 
 '''
   _____                                       _       _           _ 
@@ -130,9 +127,10 @@ def preprocessing(color, depth):
 	cv2.imwrite("depth_padded.jpg", _depth_to_heatmap(depth_padded))
 	# Normalization
 	image_mean = [0.406, 0.456, 0.485] # BGR
-	image_std  = [0.229, 0.224, 0.225]
+	image_std  = [0.225, 0.224, 0.229]
 	color_float = color_padded.astype(float)/255
 	color_norm = np.zeros_like(color_padded, dtype=float)
+	# Change to RGB
 	for i in range(3):
 		color_norm[:, :, i] = (color_float[:, :, i] - image_mean[i]) / image_std[i]
 	color_norm_ = (color_norm*255).astype(np.uint8)
@@ -197,6 +195,22 @@ def vis_affordance(predictions):
 	heatmap = cv2.applyColorMap(tmp, cv2.COLORMAP_JET)
 	return heatmap
 
+def save_heatmap_and_mixed(suck_1_prediction, suck_2_prediction, grasp_predictions, feat_paths, mixed_paths, color, iteration):
+	heatmaps   = []
+	mixed_imgs = []
+	heatmaps.append(vis_affordance(suck_1_prediction[0]))
+	heatmaps.append(vis_affordance(suck_2_prediction[0]))
+	for grasp_prediction in grasp_predictions:
+		heatmaps.append(vis_affordance(grasp_prediction))
+	for heatmap_idx in range(len(heatmaps)):
+		img_name = feat_paths[heatmap_idx] + "{:06}.jpg".format(iteration)
+		cv2.imwrite(img_name, heatmaps[heatmap_idx])
+		img_name = mixed_paths[heatmap_idx] + "{:06}.jpg".format(iteration)
+		mixed = cv2.addWeighted(color, 1.0, heatmaps[heatmap_idx], 0.4, 0)
+		mixed_imgs.append(mixed)
+		cv2.imwrite(img_name, mixed)
+	return heatmaps, mixed_imgs
+
 '''
   _____      _ _                       _       _           _ 
  |  __ \    | (_)                     | |     | |         | |
@@ -234,6 +248,18 @@ def generate_mask(depth):
 	dilate = cv2.dilate(erode, kernel, iterations=3)
 	return dilate
 
+def standarization(prediction):
+	if prediction.shape[0] is not 1:
+		for i in range(prediction.shape[0]):
+			mean = np.nanmean(prediction[i])
+			std  = np.nanstd(prediction[i])
+			prediction[i] = (prediction[i]-mean)/std
+	else:
+		mean = np.nanmean(prediction)
+		std = np.nanstd(prediction)
+		prediction = (prediction-mean)/std
+	return prediction
+
 # Choose action using epsilon-greedy policy
 def epsilon_greedy_policy(epsilon, suck_1_prediction, suck_2_prediction, grasp_prediction, depth, loggerPath, iteration):
 	explore = np.random.uniform() < epsilon # explore with probability epsilon
@@ -242,10 +268,10 @@ def epsilon_greedy_policy(epsilon, suck_1_prediction, suck_2_prediction, grasp_p
 	action_str = "suck_1"
 	pixel_index = [] # primitive index, y, x
 	out_str = ""
+	print np.max(suck_1_prediction), np.max(suck_2_prediction), np.max(grasp_prediction[0]), np.max(grasp_prediction[1]), np.max(grasp_prediction[2]), np.max(grasp_prediction[3])
 	if not explore: # Choose max Q
 		out_str += "|Exploit| "
 		primitives_max = [np.max(suck_1_prediction), np.max(suck_2_prediction), np.max(grasp_prediction)]
-		print primitives_max
 		max_q_index = np.where(primitives_max==np.max(primitives_max))[0][0]
 		if max_q_index == 0: # suck_1
 			tmp = np.where(suck_1_prediction == np.max(suck_1_prediction))
@@ -335,10 +361,10 @@ def create_argparser():
 	parser.add_argument("--epsilon", type=float, default=0.5, help="Probability to choose random action, default is 0.5")
 	parser.add_argument("--port", type=str, default="/dev/ttylight", help="Port for arduino, which controls the alram lamp, default is /dev/ttylight")
 	parser.add_argument("--buffer_size", type=int, default=1000, help="Experience buffer size, default is 1000") # N
-	parser.add_argument("--learning_freq", type=int, default=10, help="Frequency for updating behavior network, default is 10") # M
-	parser.add_argument("--updating_freq", type=int, default=40, help="Frequency for updating target network, default is 40") # C
-	parser.add_argument("--mini_batch_size", type=int, default=5, help="How many transitions should used for learning, default is 4") # K
-	parser.add_argument("--save_every", type=int, default=10, help="Every how many steps should save the model, default is 10")
+	parser.add_argument("--learning_freq", type=int, default=10, help="Frequency for updating behavior network, default is 10") # M, deprecated
+	parser.add_argument("--updating_freq", type=int, default=20, help="Frequency for updating target network, default is 20") # C
+	parser.add_argument("--mini_batch_size", type=int, default=5, help="How many transitions should used for learning, default is 5") # K
+	parser.add_argument("--save_every", type=int, default=5, help="Every how many steps should save the model, default is 5")
 	parser.add_argument("--learning_rate", type=float, default=5e-4, help="Learning rate for the trainer, default is 5e-4")
 	parser.add_argument("--densenet_lr", type=float, default=1e-4, help="Learning rate for the densenet block, default is 1e-4")
 	parser.add_argument("--run_episode", type=int, default=0, help="index for recording bag")
@@ -369,19 +395,6 @@ def parse_input(args):
 	densenet_lr = args.densenet_lr
 	return testing, run, use_cpu, model_str, buffer_str, epsilon, port, \
 	       buffer_size, learning_freq, updating_freq, mini_batch_size, save_every, learning_rate, run_episode, densenet_lr
-	
-
-def standarization(prediction):
-	if prediction.shape[0] is not 1:
-		for i in range(prediction.shape[0]):
-			mean = np.nanmean(prediction[i])
-			std  = np.nanstd(prediction[i])
-			prediction[i] = (prediction[i]-mean)/std
-	else:
-		mean = np.nanmean(prediction)
-		std = np.nanstd(prediction)
-		prediction = (prediction-mean)/std
-	return prediction
 
 def get_file_path(color_img_path_str):
 	idx = color_img_path_str[-16:]; idx = idx.replace("color_", ""); idx = idx.replace(".jpg", "")
@@ -494,20 +507,9 @@ def check_if_valid(position):
 	   (position[2] > z_lower and position[2] < z_upper):
 	   return True # valid
 	else: return False # invalid
-
-def softmax(x):
-	return np.exp(x)/np.sum(np.exp(x))
 	
 def getModelCapacity(model):
 	return sum(p.numel() for p in model.parameters() if p.requires_grad)
-	
-def print_action(action_str, pixel_index, point):
-	print "Take action [\033[1;31m%s\033[0m] at (%d, %d) -> (%f, %f, %f)" %(action_str, \
-	                                                                        pixel_index[1], pixel_index[2], \
-	                                                                        point[0], point[1], point[2])
-
-def point_np_to_ros(point):
-	return Point(point[0], point[1], point[2])
 	
 def wrap_strings(image_path, depth_path, iteration):
 	color_name = image_path + "color_{:06}.jpg".format(iteration)
@@ -532,22 +534,6 @@ def get_action_info(pixel_index):
 		action_str = "grasp"; rotate_idx = pixel_index[0]-2
 	return action_str, rotate_idx
 
-def save_heatmap_and_mixed(suck_1_prediction, suck_2_prediction, grasp_predictions, feat_paths, mixed_paths, color, iteration):
-	heatmaps   = []
-	mixed_imgs = []
-	heatmaps.append(vis_affordance(suck_1_prediction[0]))
-	heatmaps.append(vis_affordance(suck_2_prediction[0]))
-	for grasp_prediction in grasp_predictions:
-		heatmaps.append(vis_affordance(grasp_prediction))
-	for heatmap_idx in range(len(heatmaps)):
-		img_name = feat_paths[heatmap_idx] + "{:06}.jpg".format(iteration)
-		cv2.imwrite(img_name, heatmaps[heatmap_idx])
-		img_name = mixed_paths[heatmap_idx] + "{:06}.jpg".format(iteration)
-		mixed = cv2.addWeighted(color, 1.0, heatmaps[heatmap_idx], 0.4, 0)
-		mixed_imgs.append(mixed)
-		cv2.imwrite(img_name, mixed)
-	return heatmaps, mixed_imgs
-	
 def parse_string(input_str):
 	def _substring(input_str, key, length):
 		pos = input_str.find(key)
@@ -557,7 +543,14 @@ def parse_string(input_str):
 	print "Sample at episode: {} iteration: {}".format(episode, iter_)
 	return episode, iter_
 	
-# Reward shaping
+'''
+  ____                                         _ 
+ |  _ \    ___  __      __   __ _   _ __    __| |
+ | |_) |  / _ \ \ \ /\ / /  / _` | | '__|  / _` |
+ |  _ <  |  __/  \ V  V /  | (_| | | |    | (_| |
+ |_| \_\  \___|   \_/\_/    \__,_| |_|     \__,_|
+                                                 
+'''
 def reward_judgement(reward_unit, action_valid, action_success):
 	if not action_valid:
 		return -3*reward_unit # Invalid
@@ -565,3 +558,41 @@ def reward_judgement(reward_unit, action_valid, action_success):
 		return reward_unit # Valid and success
 	else:
 		return -reward_unit # Valid and failed
+		
+'''
+   ____                                               _                 
+  / ___|   ___    _ __   __   __   ___   _ __   ___  (_)   ___    _ __  
+ | |      / _ \  | '_ \  \ \ / /  / _ \ | '__| / __| | |  / _ \  | '_ \ 
+ | |___  | (_) | | | | |  \ V /  |  __/ | |    \__ \ | | | (_) | | | | |
+  \____|  \___/  |_| |_|   \_/    \___| |_|    |___/ |_|  \___/  |_| |_|
+                                                                        
+'''
+
+def point_np_to_ros(point):
+	return Point(point[0], point[1], point[2])
+
+'''
+  __  __           _     _     
+ |  \/  |   __ _  | |_  | |__  
+ | |\/| |  / _` | | __| | '_ \ 
+ | |  | | | (_| | | |_  | | | |
+ |_|  |_|  \__,_|  \__| |_| |_|
+                               
+'''
+
+def softmax(x):
+	return np.exp(x)/np.sum(np.exp(x))
+	
+'''
+  ___            __                                      _     _                 
+ |_ _|  _ __    / _|   ___    _ __   _ __ ___     __ _  | |_  (_)   ___    _ __  
+  | |  | '_ \  | |_   / _ \  | '__| | '_ ` _ \   / _` | | __| | |  / _ \  | '_ \ 
+  | |  | | | | |  _| | (_) | | |    | | | | | | | (_| | | |_  | | | (_) | | | | |
+ |___| |_| |_| |_|    \___/  |_|    |_| |_| |_|  \__,_|  \__| |_|  \___/  |_| |_|
+                                                                                 
+'''
+
+def print_action(action_str, pixel_index, point):
+	print "Take action [\033[1;31m%s\033[0m] at (%d, %d) -> (%f, %f, %f)" %(action_str, \
+	                                                                        pixel_index[1], pixel_index[2], \
+	                                                                        point[0], point[1], point[2])
