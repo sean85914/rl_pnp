71a299ff41023639888dbae79e278c51304f9066
sean85914 <sean19960914@gmail.com>
2020-03-09 02:35:03 +0800

    log statistics

--- src/grasp_suck/src/bag_to_data.cpp
+++ src/grasp_suck/src/bag_to_data.cpp
@@ -47,6 +47,10 @@ Usage: ./bag_to_video bag_name output_video_name [speed]\n\033[0m";
   auto bag_start_ts = view.getBeginTime();
   std::cout << "Bag duration: " << bag_duration << " seconds\n";
   std::cout << "Writing...\n";
+  int total_iter = 0, invalid_iter = 0, total_success = 0, \
+      gripper_usage = 0, gripper_success = 0, \
+      suction_1_usage = 0, suction_1_success = 0, \
+      suction_2_usage = 0, suction_2_success = 0;
   // Start reading
   auto s_ts = std::chrono::high_resolution_clock::now();
   foreach(const rosbag::MessageInstance m, view){
@@ -54,11 +58,36 @@ Usage: ./bag_to_video bag_name output_video_name [speed]\n\033[0m";
       arm_operation::execution::ConstPtr data_ptr = m.instantiate<arm_operation::execution>();
       has_data = true;
       double time_from_start = (data_ptr->header.stamp-bag_start_ts).toSec();
+      total_iter += 1;
       file << data_ptr->iteration << "\t" 
            << data_ptr->header.stamp << "\t" 
            << time_from_start << "\t" 
            << data_ptr->primitive << "\t"
            << (data_ptr->is_success?"success":"fail") << "\n";
+      if(data_ptr->primitive=="invalid"){
+        invalid_iter+=1;
+      }
+      else if(data_ptr->primitive=="suck_1"){
+        suction_1_usage+=1;
+        if(data_ptr->is_success){
+          suction_1_success+=1;
+          total_success+=1;
+        }
+      }
+      else if(data_ptr->primitive=="suck_2"){
+        suction_2_usage+=1;
+        if(data_ptr->is_success){
+          suction_2_success+=1;
+          total_success+=1;
+        }
+      }else{
+        gripper_usage+=1;
+        if(data_ptr->is_success){
+          gripper_success+=1;
+          total_success+=1;
+        }
+      }
+      
     }
   }
   if(!has_data){
@@ -66,6 +95,16 @@ Usage: ./bag_to_video bag_name output_video_name [speed]\n\033[0m";
     file.close();
     exit(EXIT_FAILURE);
   }
+  file << "========================================================================\n"
+       << "Run time: \t" << bag_duration << "\n"
+       << "Invalid action rate: \t" << float(invalid_iter)/total_iter << "\n"
+       << "Success rate: \t" << float(total_success)/total_iter << "\n"
+       << "Gripper success rate: \t" << float(gripper_success)/gripper_usage << "\n"
+       << "Suction 1 success rate: \t" << float(suction_1_success)/suction_1_usage << "\n"
+       << "Suction 2 success rate: \t" << float(suction_2_success)/suction_2_usage << "\n"
+       << "Gripper usage rate: \t" << float(gripper_usage)/total_iter << "\n"
+       << "Suction 1 usage rate: \t" << float(suction_1_usage)/total_iter << "\n"
+       << "Suction 2 usage rate: \t" << float(suction_2_usage)/total_iter << "\n";
   auto e_ts = std::chrono::high_resolution_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(e_ts-s_ts).count();
   file.close();
