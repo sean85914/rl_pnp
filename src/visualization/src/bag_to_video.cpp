2db5ef078704df5fac801a5d5108a4ab52e1d1c3
sean85914 <sean19960914@gmail.com>
2020-03-14 23:16:46 +0800

    count image number first

--- src/visualization/src/bag_to_video.cpp
+++ src/visualization/src/bag_to_video.cpp
@@ -1,7 +1,7 @@
 // STD
 #include <experimental/filesystem>
 #include <chrono>
-#include <utility>
+#include <queue>
 #include <thread>
 // Boost
 #include <boost/foreach.hpp>
@@ -20,33 +20,31 @@
 
 #define foreach BOOST_FOREACH
 
-typedef std::pair<cv::Mat, ros::Time> data_pair;
 bool read_complete = false;
 int width, height;
-double speed = 1.0;
+double fps;
 std::string out_name;
-boost::shared_mutex _access;
 
-void write_video(std::vector<data_pair> &data_vec){
-  while(data_vec.size()<2){usleep(10000);} // Wait until more than 2 data
-  double estimated_fps = 1/(data_vec[1].second - data_vec[0].second).toSec();
-  cv::VideoWriter video(out_name, CV_FOURCC('H', '2', '6', '4'), estimated_fps*speed, cv::Size(width, height));
+
+void write_video(std::queue<cv::Mat> &data_vec){
+  while(data_vec.empty()){} // Wait until data in the queue
+  cv::VideoWriter video(out_name, CV_FOURCC('H', '2', '6', '4'), fps, cv::Size(width, height));
   while(true){
     if(read_complete and data_vec.empty()) break; // Stop
     if(data_vec.empty()) continue; // Consume too fast
-    video << data_vec[0].first;
-    data_vec.erase(data_vec.begin());
+    video << data_vec.front();
+    data_vec.pop();
     if(read_complete){
       std::cout << "\r" << data_vec.size() << " images remaining...";
     }
   }
 }
 
-
 int main(int argc, char** argv)
 {
   // Parse input argument
   bool has_data = false, is_compressed;
+  double speed = 1.0;
   std::string topic_name="";
   if(argc<3){
     std::cout << "\033[1;31mInsufficient input arguments\n\
@@ -79,14 +77,47 @@ Usage: ./bag_to_video bag_name output_video_name [speed]\n\033[0m";
   }
   rosbag::View view(bag);
   cv_bridge::CvImagePtr cv_bridge_ptr;
-  std::vector<data_pair> data_vec;
+  std::queue<cv::Mat> data_queue;
   double bag_duration = (view.getEndTime() - view.getBeginTime()).toSec();
   std::cout << "Bag duration: " << bag_duration << " seconds\n";
   std::cout << "Converting...\n";
   // Start writing thread
-  std::thread writing_thread(write_video, std::ref(data_vec));
+  std::thread writing_thread(write_video, std::ref(data_queue));
   // Start reading
   auto s_ts = std::chrono::high_resolution_clock::now();
+  // Counting number of images
+  int img_cnt = 0;
+  foreach(const rosbag::MessageInstance m, view){
+    if(m.getDataType()=="sensor_msgs/Image" or \
+       m.getDataType()=="sensor_msgs/CompressedImage"){
+      if(img_cnt==0){ // Only convert if first image 
+        if(m.getDataType()=="sensor_msgs/Image"){
+          sensor_msgs::Image::ConstPtr img_ptr = m.instantiate<sensor_msgs::Image>();
+          try{
+            cv_bridge_ptr = cv_bridge::toCvCopy(img_ptr);
+          } catch(cv_bridge::Exception& e){
+            std::cout << "\033[1;33mcv_bridge exception: " << e.what() << "\033[0m\n";
+            exit(EXIT_FAILURE);
+          }
+          width = cv_bridge_ptr->image.cols;
+          height = cv_bridge_ptr->image.rows;
+        }else{
+          sensor_msgs::CompressedImage::ConstPtr img_ptr = m.instantiate<sensor_msgs::CompressedImage>();
+          try{
+            cv_bridge_ptr = cv_bridge::toCvCopy(img_ptr);
+          } catch(cv_bridge::Exception& e){
+            std::cout << "\033[1;33mcv_bridge exception: " << e.what() << "\033[0m\n";
+            exit(EXIT_FAILURE);
+          }
+          width = cv_bridge_ptr->image.cols;
+          height = cv_bridge_ptr->image.rows;
+        }
+      }
+      ++img_cnt;
+    }
+  }
+  double estimated_fps = img_cnt/bag_duration;
+  fps = estimated_fps*speed;
   foreach(const rosbag::MessageInstance m, view){
     if(m.getDataType()=="sensor_msgs/Image" or \
        m.getDataType()=="sensor_msgs/CompressedImage"){
@@ -118,16 +149,13 @@ Usage: ./bag_to_video bag_name output_video_name [speed]\n\033[0m";
       // Make sure is the same topic
       if(m.getTopic()!=topic_name) continue;
       double read_ratio = ((m.getTime() - view.getBeginTime()).toSec())/bag_duration;
-      width = cv_bridge_ptr->image.cols;
-      height = cv_bridge_ptr->image.rows;
       cv::Mat img;
       if(is_compressed) img = cv_bridge_ptr->image; // Don't have to convert, VideoWriter expected BGR image as input
       else{
         cv::cvtColor(cv_bridge_ptr->image, img, CV_RGB2BGR);
       }
       printf("\r[%05.1f%%]", read_ratio*100);
-      data_pair pair = std::make_pair(img, m.getTime());
-      data_vec.push_back(pair);
+      data_queue.push(img);
     }
   }
   read_complete = true;
