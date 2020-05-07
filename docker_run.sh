542afe7c8bdb604099f0ffdf0d4888395a9eef8b
sean85914 <sean19960914@gmail.com>
2020-05-07 12:00:58 +0800

    update docker files and add run docker shell script

---
+++ docker_run.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+CONTAINER_NAME="ddqn_bin_picking"
+
+# Check if given input arguments
+if [ $# -eq 0 ]
+then
+  echo -e "\033[1;31mNo input arguments given, exiting...\033[0m"
+  return
+fi
+
+xhost +local:docker
+
+if [ "$1" == "same" ]
+then
+  docker exec -it --privileged ${CONTAINER_NAME} bash
+fi
+
+# Check if valid cuda version
+if [ `echo $LD_LIBRARY_PATH | grep -c "cuda-9.*" ` -gt 0 ]
+then
+  echo -e "\033[1;35mFind Cuda version 9.0\033[0m"
+  CUDA_TAG="cuda9.0"
+elif [ `echo $LD_LIBRARY_PATH | grep -c "cuda-10.*" ` -gt 0 ]
+then
+  echo "\033[1;35mFind Cuda version 10.0\033[0m"
+  CUDA_TAG="cuda10.0"
+fi
+
+# Check if $CUDA_TAG set
+if [ -z "$CUDA_TAG" ]
+then
+  echo "\033[1;31mInvalid Cuda version (we only provided with version 9.* and 10.*) or no Cuda Installed\033[0m"
+  return
+fi
+
+if [[ "$1" == "run" ]]
+then
+  echo -e "\033[1;33mOpening container with name ${CONTAINER_NAME}\033[0m"
+  docker run --name ${CONTAINER_NAME} --rm -it --net=host --privileged -v /dev:/dev \
+  --env="DISPLAY"="$DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /tmp/.X11-unix/:/tmp/.X11-unix \
+  -v /home/$USER/rl_pnp:/home/$USER/rl_pnp \
+  --env "XAUTHORITY=$XAUTH" \
+  -v "XAUTHORITY=$XAUTH" \
+  --runtime=nvidia \
+  --device=/dev/dri:/dev/dri \
+  --device=/dev/nvhost-gpu \
+  --device=/dev/nvhost-as-gpu \
+  --device=/dev/nvhost-ctrl \
+  --device=/dev/nvhost-ctrl-gpu \
+  --device=/dev/nvhost-prof-gpu \
+  --device=/dev/nvmap \
+  --device=/dev/ttyUSB* \
+  --device=/dev/ttyACM* \
+  -v /dev/bus/usb:/dev/bus/usb \
+  sean85914/ddqn_bin_picking:cuda9.0 bash
+fi
--- test_thres_no_moved.sh
+++
@@ -1,9 +0,0 @@
-BEGIN=1
-END=100
-for ((i=$BEGIN; i<=$END; i++))
-do
-  rosservice call /get_reward/set_prior
-  rosservice call /get_reward/set_posterior
-  rosservice call /get_reward/get_result "data: false"
-  echo "$i"
-done
