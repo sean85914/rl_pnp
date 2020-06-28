199c8fa7b1c7b6a76624f22aeb291e7db59f7c8d
sean85914 <sean19960914@gmail.com>
2020-06-29 00:05:31 +0800

    add state vector to action

--- src/grasp_suck/src/generate_hdf5.py
+++ src/grasp_suck/src/generate_hdf5.py
@@ -16,8 +16,14 @@ def conversion(path_to_exp, hdf5_dir, dir):
 	result = np.loadtxt(path_to_exp+dir+"/action_result.csv", delimiter=",")
 	target = np.loadtxt(path_to_exp+dir+"/action_target.csv", delimiter=",")
 	empty = np.loadtxt(path_to_exp+dir+"/empty_result.csv", delimiter=",")
-	reward_unit = 5.0
-	# attribute
+	position = np.loadtxt(path_to_exp+dir+"/position.csv", delimiter=",")
+	'''
+	Success = +R
+	Fail = -R
+	Invalud = -3R
+	'''
+	reward_unit = 5.0 # R
+	# some static attribute counters
 	total_success = 0
 	total_fail = 0
 	total_invalid = 0
@@ -38,6 +44,8 @@ def conversion(path_to_exp, hdf5_dir, dir):
 		action = target[i]
 		invalid = (primitive[i]==-1)
 		fail = (result[i]==0)
+		operating_position = position[i]
+		angle = 0.0
 		# hdf5
 		grp = f.create_group("iter_{:04}".format(i))
 		state = grp.create_group("state")
@@ -65,10 +73,15 @@ def conversion(path_to_exp, hdf5_dir, dir):
 		else:
 			gripper_used+=1
 			gripper_angle[int(action[0])-2]+=1
+			angle = np.radians(-90+45*(int(action[0])-2))
 			if not fail:
 				gripper_success+=1
-		_ = grp.create_dataset("action", action.shape, dtype=np.uint8)
-		grp["action"][:] = action
+		operating_vector = np.append(operating_position, angle) # x, y, z, theta (angle in radian)
+		_ = grp.create_dataset("action", operating_vector.shape, dtype=np.float32)
+		grp["action"][:] = operating_vector
+		_.attrs.create("primitive_id", action[0], dtype=int)
+		_.attrs.create("pixel_x", action[2], dtype=int)
+		_.attrs.create("pixel_y", action[1], dtype=int)
 		_ = grp.create_dataset("reward", (1, ), dtype=np.int8)
 		if invalid:
 			grp["reward"][:] = -3*reward_unit
@@ -78,21 +91,34 @@ def conversion(path_to_exp, hdf5_dir, dir):
 			total_fail += 1
 		else:
 			grp["reward"][:] = reward_unit
-			total_success += 1	
-	# attributes
-	f.attrs.create("total_iter", total_iter, dtype=np.uint16)
-	f.attrs.create("total_success", total_success, dtype=np.uint16)
-	f.attrs.create("total_fail", total_fail, dtype=np.uint16)
-	f.attrs.create("total_invalid", total_invalid, dtype=np.uint16)
-	f.attrs.create("total_empty", total_empty, dtype=np.uint16)
-	f.attrs.create("tool_gripper_used", gripper_used, dtype=np.uint16)
-	f.attrs.create("tool_small_suction_used", small_suction_used, dtype=np.uint16)
-	f.attrs.create("tool_medium_suction_used", medium_suction_used, dtype=np.uint16)
-	f.attrs.create("tool_gripper_success", gripper_success, dtype=np.uint16)
-	f.attrs.create("tool_small_suction_success", small_suction_success, dtype=np.uint16)
-	f.attrs.create("tool_medium_suction_success", medium_suction_success, dtype=np.uint16)
-	f.attrs.create("tool_gripper_angle_used", gripper_angle, dtype=np.uint16)
-	f.attrs.create("reward_unit", reward_unit, dtype=np.float16)
+			total_success += 1
+	# metadata, and its attributes
+	meta = f.create_group("metadata")
+	meta.attrs['action_space'] = "x y z theta"
+	meta.attrs['background'] = "lab"
+	meta.attrs['bin_insert'] = "none"
+	meta.attrs['bin_type'] = "plastic blue bin"
+	meta.attrs['camera_type'] =  "Intel RealSense D435"
+	meta.attrs['gripper'] = "XYZ pneumatic changeable tools"
+	meta.attrs['high_bound'] = [1.02, -0.24, 0.2]
+	meta.attrs['low_bound'] =  [0.72, -0.54, -0.1]
+	meta.attrs['object_classes'] = "products"
+	meta.attrs['policy'] = "epsilon-greedy selsection among network predictions"
+	meta.attrs['robot'] = "ABB IRB1660ID"
+	# top-level attributes
+	f.attrs['total_iter'] = total_iter
+	f.attrs['total_success'] = total_success
+	f.attrs['total_fail'] = total_fail
+	f.attrs['total_invalid'] = total_invalid
+	f.attrs['total_empty'] = total_empty
+	f.attrs['tool_gripper_used'] = gripper_used
+	f.attrs['tool_small_suction_used'] = small_suction_used
+	f.attrs['tool_medium_suction_used'] = medium_suction_used
+	f.attrs['tool_gripper_success'] = gripper_success
+	f.attrs['tool_small_suction_success'] = small_suction_success
+	f.attrs['tool_medium_suction_success'] = medium_suction_success
+	f.attrs['tool_gripper_angle_used'] = gripper_angle
+	f.attrs['reward_unit'] = reward_unit
 	f.close()
 	print "{}: conversion takes {} seconds | {} experiences added".format(filename, time.time()-ts, total_iter)
 	
