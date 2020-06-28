b93342a11f0123251bd3968fa6de737311677984
sean85914 <sean19960914@gmail.com>
2020-06-28 23:34:32 +0800

    download baseline models

--- download_model.sh
+++ download_model.sh
@@ -1,4 +1,7 @@
 current_path=$PWD
 cd src/grasp_suck/src
 gdown --id 1QDelIRjn3BZ3LnUc_7374pwDtLNxhqkD
+cd baseline
+gdown --id 1HyfTVcXmGF21KrSlhDdX5oFOjiZChIZT
+gdown --id 1vyisDaxEYsKWhlkxbyjayWnIUwjX4LR6
 cd $current_path
